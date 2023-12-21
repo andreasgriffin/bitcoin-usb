33b0094c60b94f527deadad9aca09c76621148cf
andreasgriffin <andreasgriffin@proton.me>
2023-12-21 11:39:05 +0100

    pre-commit install

--- bitcoin_usb/address_types.py
+++ bitcoin_usb/address_types.py
@@ -5,13 +5,19 @@ logger = logging.getLogger(__name__)
 import bdkpython as bdk
 from typing import Callable, Dict, List, Tuple
 
-from hwilib.descriptor import parse_descriptor, Descriptor, PubkeyProvider, WPKHDescriptor, WSHDescriptor, PKHDescriptor, MultisigDescriptor, TRDescriptor, SHDescriptor
-from hwilib.key import  KeyOriginInfo, HARDENED_FLAG
-from hwilib.common import  AddressType as HWIAddressType
-
-
-
-
+from hwilib.descriptor import (
+    parse_descriptor,
+    Descriptor,
+    PubkeyProvider,
+    WPKHDescriptor,
+    WSHDescriptor,
+    PKHDescriptor,
+    MultisigDescriptor,
+    TRDescriptor,
+    SHDescriptor,
+)
+from hwilib.key import KeyOriginInfo, HARDENED_FLAG
+from hwilib.common import AddressType as HWIAddressType
 
 
 # https://bitcoin.design/guide/glossary/address/
@@ -31,7 +37,7 @@ class AddressType:
     ) -> None:
         self.name = name
         self.is_multisig = is_multisig
-        self.key_origin:Callable = key_origin
+        self.key_origin: Callable = key_origin
         self.bdk_descriptor_secret = bdk_descriptor_secret
         self.info_url = info_url
         self.description = description
@@ -41,7 +47,7 @@ class AddressType:
     def clone(self):
         return AddressType(
             name=self.name,
-            is_multisig= self.is_multisig,
+            is_multisig=self.is_multisig,
             key_origin=self.key_origin,
             bdk_descriptor_secret=self.bdk_descriptor_secret,
             info_url=self.info_url,
@@ -52,11 +58,13 @@ class AddressType:
 
     def __str__(self):
         return str(self.name)
-    
+
     def __repr__(self):
         return f"AddressType({self.__dict__})"
 
-    def get_bip32_path(self, network:bdk.Network, keychain:bdk.KeychainKind,  address_index:int )->str:
+    def get_bip32_path(
+        self, network: bdk.Network, keychain: bdk.KeychainKind, address_index: int
+    ) -> str:
         return f"{self.key_origin(network)}/{0 if keychain == bdk.KeychainKind.EXTERNAL else 1}/{address_index}"
 
 
@@ -79,7 +87,7 @@ class AddressTypes:
         bdk_descriptor_secret=bdk.Descriptor.new_bip49,
         info_url="https://learnmeabitcoin.com/technical/derivation-paths",
         description="Nested (single sig) addresses that look like 3addresses",
-        hwi_descriptor_classes= (SHDescriptor, WPKHDescriptor),
+        hwi_descriptor_classes=(SHDescriptor, WPKHDescriptor),
     )
     p2wpkh = AddressType(
         "Single Sig (SegWit/p2wpkh)",
@@ -108,7 +116,7 @@ class AddressTypes:
         bdk_descriptor_secret=None,
         info_url="https://github.com/bitcoin/bips/blob/master/bip-0048.mediawiki",
         description="Nested (multi sig) addresses that look like 3addresses",
-        hwi_descriptor_classes= (SHDescriptor  ,WSHDescriptor, MultisigDescriptor) ,
+        hwi_descriptor_classes=(SHDescriptor, WSHDescriptor, MultisigDescriptor),
     )
     p2wsh = AddressType(
         "Multi Sig (SegWit/p2wsh)",
@@ -117,157 +125,162 @@ class AddressTypes:
         bdk_descriptor_secret=None,
         info_url="https://github.com/bitcoin/bips/blob/master/bip-0048.mediawiki",
         description="SegWit (multi sig) addresses that look like bc1addresses",
-        hwi_descriptor_classes= (WSHDescriptor,MultisigDescriptor),
+        hwi_descriptor_classes=(WSHDescriptor, MultisigDescriptor),
     )
 
 
-
 def get_address_type_dicts() -> Dict[str, AddressType]:
-    return {
-        k:v
-        for k, v in AddressTypes.__dict__.items()
-        if (not k.startswith("_")) 
-    }
-    
+    return {k: v for k, v in AddressTypes.__dict__.items() if (not k.startswith("_"))}
+
+
 def get_address_types() -> List[AddressType]:
     return get_address_type_dicts().values()
 
-    
-    
-def get_hwi_address_type(address_type:AddressType)->HWIAddressType:
+
+def get_hwi_address_type(address_type: AddressType) -> HWIAddressType:
     # see https://hwi.readthedocs.io/en/latest/usage/api-usage.html#hwilib.common.AddressType
-    if address_type.name  in [AddressTypes.p2pkh.name]:
+    if address_type.name in [AddressTypes.p2pkh.name]:
         return HWIAddressType.LEGACY
-    if address_type.name  in [AddressTypes.p2wpkh.name ]:
+    if address_type.name in [AddressTypes.p2wpkh.name]:
         return HWIAddressType.WIT
-    if address_type.name  in [AddressTypes.p2sh_p2wpkh.name, AddressTypes.p2sh_p2wsh.name ]:
+    if address_type.name in [
+        AddressTypes.p2sh_p2wpkh.name,
+        AddressTypes.p2sh_p2wsh.name,
+    ]:
         return HWIAddressType.SH_WIT
-    if address_type.name  in [AddressTypes.p2tr.name ]:
+    if address_type.name in [AddressTypes.p2tr.name]:
         return HWIAddressType.TAP
-    
-        
 
 
-
-class SignerInfo():
+class SignerInfo:
     def __init__(
         self,
-        xpub:str,
-        fingerprint:str,
-        key_origin: str, 
-        derivation_path: str, 
+        xpub: str,
+        fingerprint: str,
+        key_origin: str,
+        derivation_path: str,
     ) -> None:
         self.xpub = xpub
         self.fingerprint = fingerprint
         # key_origin example: "m/84h/1h/0h"
-        assert key_origin.startswith('m/')
-        self.key_origin = key_origin 
+        assert key_origin.startswith("m/")
+        self.key_origin = key_origin
         # derivation_path example "/0/*"
-        assert derivation_path.startswith('/')
+        assert derivation_path.startswith("/")
         self.derivation_path = derivation_path
 
     @classmethod
-    def from_hwi(pubkey_provider: PubkeyProvider)-> "SignerInfo":
-        return SignerInfo(xpub = pubkey_provider.pubkey,
-                          fingerprint=pubkey_provider.origin.fingerprint.hex(),
-                          key_origin=pubkey_provider.origin.get_derivation_path(),
-                          derivation_path=pubkey_provider.deriv_path)
+    def from_hwi(pubkey_provider: PubkeyProvider) -> "SignerInfo":
+        return SignerInfo(
+            xpub=pubkey_provider.pubkey,
+            fingerprint=pubkey_provider.origin.fingerprint.hex(),
+            key_origin=pubkey_provider.origin.get_derivation_path(),
+            derivation_path=pubkey_provider.deriv_path,
+        )
 
-    def to_hwi_pubkey_provider(self)-> PubkeyProvider:
-        hwi_key_origin= self.key_origin.replace('m/', f"{self.fingerprint}/")
-        provider = PubkeyProvider(origin= hwi_key_origin, 
-               pubkey=self.xpub,
-               deriv_path=self.derivation_path)
+    def to_hwi_pubkey_provider(self) -> PubkeyProvider:
+        hwi_key_origin = self.key_origin.replace("m/", f"{self.fingerprint}/")
+        provider = PubkeyProvider(
+            origin=hwi_key_origin, pubkey=self.xpub, deriv_path=self.derivation_path
+        )
         return provider
+
 
 def _get_descriptor_instances(descriptor):
     "Returns the linear chain of chained descriptors . Multiple subdescriptors return an error"
-    assert len(descriptor.subdescriptors ) <= 1
+    assert len(descriptor.subdescriptors) <= 1
     if descriptor.subdescriptors:
         result = [descriptor]
-        for subdescriptor in  descriptor.subdescriptors:
-            result +=  _get_descriptor_instances(subdescriptor) 
+        for subdescriptor in descriptor.subdescriptors:
+            result += _get_descriptor_instances(subdescriptor)
         return result
     else:
         return [descriptor]
-    
-    
-def _find_matching_address_type(instance_tuple, address_types:List[AddressType]):
+
+
+def _find_matching_address_type(instance_tuple, address_types: List[AddressType]):
     for address_type in address_types:
-        if len(instance_tuple) == len(address_type.hwi_descriptor_classes) and all( isinstance(i, c)    for i,c in zip(instance_tuple, address_type.hwi_descriptor_classes)):
+        if len(instance_tuple) == len(address_type.hwi_descriptor_classes) and all(
+            isinstance(i, c)
+            for i, c in zip(instance_tuple, address_type.hwi_descriptor_classes)
+        ):
             return address_type
     return None
 
 
 class DescriptorInfo:
-    def __init__(self, address_type:AddressType, signer_infos:List[SignerInfo], threshold=1) -> None:
+    def __init__(
+        self, address_type: AddressType, signer_infos: List[SignerInfo], threshold=1
+    ) -> None:
         self.address_type = address_type
         self.signer_infos = signer_infos
         self.threshold = threshold
 
+
 def public_descriptor_info(descriptor_str: str) -> DescriptorInfo:
     "gets the xpub (not xpriv) information"
 
+    hwi_descriptor = parse_descriptor(descriptor_str)
 
-    hwi_descriptor = parse_descriptor(descriptor_str)    
-    
     # first we need to identify the address type
-    address_type = _find_matching_address_type(_get_descriptor_instances(hwi_descriptor), get_address_types())        
+    address_type = _find_matching_address_type(
+        _get_descriptor_instances(hwi_descriptor), get_address_types()
+    )
     if not address_type:
-        raise ValueError(f"descriptor {descriptor_str} cannot be matched to known template" )
-    
-    
-    
-    
+        raise ValueError(
+            f"descriptor {descriptor_str} cannot be matched to known template"
+        )
 
     # get the     pubkey_providers, by "walking to the end of desciptors"
     threshold = 1
     subdescriptor = hwi_descriptor
-    for descritptor_class  in address_type.hwi_descriptor_classes:
+    for descritptor_class in address_type.hwi_descriptor_classes:
         # just double checking that _find_matching_address_type did its job correctly
         assert isinstance(subdescriptor, descritptor_class)
         subdescriptor = subdescriptor.subdescriptors[0]
-        
+
         if descritptor_class == MultisigDescriptor:
             # last descriptor is a multisig
             threshold = subdescriptor.thresh
-            
+
     pubkey_providers = subdescriptor.pubkeys
 
-    
+    return DescriptorInfo(
+        address_type=address_type,
+        signer_infos=[
+            SignerInfo.from_hwi(pubkey_provider) for pubkey_provider in pubkey_providers
+        ],
+        threshold=threshold,
+    )
 
-    return DescriptorInfo(address_type=address_type,signer_infos=[SignerInfo.from_hwi(pubkey_provider) for pubkey_provider in pubkey_providers] ,threshold= threshold )  
-
-
-
-
-
-    
-    
-    
 
 def make_multisig_descriptor(
     address_type: AddressType, threshold: int, descriptors: List[bdk.Descriptor]
 ) -> bdk.Descriptor:
     "Takes in single sig descriptors and creates a multisig descritpr out of it"
-    
-    assert address_type.is_multisig 
+
+    assert address_type.is_multisig
     assert descriptors
-    
+
     # check all descriptors have the same network
-    assert all([descriptor.network == descriptors[0].network for descriptor in descriptors]), "Descriptors with different networks given"
-    
+    assert all(
+        [descriptor.network == descriptors[0].network for descriptor in descriptors]
+    ), "Descriptors with different networks given"
+
     hwi_pubkey_providers = []
     for descriptor in descriptors:
-        hwi_pubkey_provider = _descriptor_to_hwi_pubkey_providers(descriptor.as_string_private())
-        if not hwi_pubkey_provider or len(hwi_pubkey_provider)>=2:
+        hwi_pubkey_provider = _descriptor_to_hwi_pubkey_providers(
+            descriptor.as_string_private()
+        )
+        if not hwi_pubkey_provider or len(hwi_pubkey_provider) >= 2:
             # cannot handle. The argument descriptors are only singlesig descriptors
             return
         hwi_pubkey_providers.append(hwi_pubkey_provider[0])
-    
 
-    multisig_hwi_descriptor =   address_type.hwi_descriptor_classes(pubkeys=hwi_pubkey_providers, thresh=threshold, is_sorted=True)
-    
-    return bdk.Descriptor(multisig_hwi_descriptor.to_string(), network=descriptors[0].network)
+    multisig_hwi_descriptor = address_type.hwi_descriptor_classes(
+        pubkeys=hwi_pubkey_providers, thresh=threshold, is_sorted=True
+    )
 
-
+    return bdk.Descriptor(
+        multisig_hwi_descriptor.to_string(), network=descriptors[0].network
+    )
--- bitcoin_usb/device.py
+++ bitcoin_usb/device.py
@@ -4,7 +4,7 @@ import threading
 import hwilib.commands as hwi_commands
 from hwilib.common import Chain
 from hwilib.psbt import PSBT
-from hwilib.descriptor import  Descriptor, PubkeyProvider
+from hwilib.descriptor import Descriptor, PubkeyProvider
 from usb1 import USBError
 
 logger = logging.getLogger(__name__)
@@ -12,21 +12,24 @@ logger = logging.getLogger(__name__)
 import bdkpython as bdk
 
 from typing import *
-from .address_types import AddressType, AddressTypes, SignerInfo, get_hwi_address_type, public_descriptor_info
+from .address_types import (
+    AddressType,
+    AddressTypes,
+    SignerInfo,
+    get_hwi_address_type,
+    public_descriptor_info,
+)
 from hwilib.descriptor import MultisigDescriptor as HWIMultisigDescriptor
 
 
-
-
-class Device:    
-    def __init__(self, enumerate_device:    List[Dict[str, Any]], network:bdk.Network): 
+class Device:
+    def __init__(self, enumerate_device: List[Dict[str, Any]], network: bdk.Network):
         self.network = network
         self.enumerate_device = enumerate_device
         self.lock = threading.Lock()
         self.client = None
-        
-        
-    def bdknetwork_to_chain(self, network:bdk.Network):
+
+    def bdknetwork_to_chain(self, network: bdk.Network):
         if network.BITCOIN:
             return Chain.MAIN
         if network.REGTEST:
@@ -34,15 +37,16 @@ class Device:
         if network.SIGNET:
             return Chain.SIGNET
         if network.TESTNET:
-            return Chain.TEST          
-        
+            return Chain.TEST
+
     def __enter__(self):
         self.lock.acquire()
-        self.client = hwi_commands.get_client(device_type=self.enumerate_device['type'], 
-                                                      device_path=self.enumerate_device['path'],
-                                                      chain=self.bdknetwork_to_chain(self.network) )
-        return self 
-        
+        self.client = hwi_commands.get_client(
+            device_type=self.enumerate_device["type"],
+            device_path=self.enumerate_device["path"],
+            chain=self.bdknetwork_to_chain(self.network),
+        )
+        return self
 
     def __exit__(self, exc_type, exc_value, traceback):
         if self.client:
@@ -52,45 +56,58 @@ class Device:
         if exc_type is not None:
             print(f"An exception occurred: {exc_value}")
 
-            
     def get_fingerprint(self) -> str:
-        return self.client.get_master_fingerprint().hex() 
-        
-    def get_xpubs(self)->Dict[AddressTypes, str]:
+        return self.client.get_master_fingerprint().hex()
+
+    def get_xpubs(self) -> Dict[AddressTypes, str]:
         xpubs = {}
         for s in vars(AddressTypes):
-            if s.startswith('_'):
+            if s.startswith("_"):
                 continue
             address_type = getattr(AddressTypes, s)
-            xpubs[address_type] = self.client.get_pubkey_at_path(address_type.key_origin(self.network)).to_string()
+            xpubs[address_type] = self.client.get_pubkey_at_path(
+                address_type.key_origin(self.network)
+            ).to_string()
         return xpubs
-            
-            
-    def sign_psbt(self, psbt:bdk.PartiallySignedTransaction) -> bdk.PartiallySignedTransaction:
+
+    def sign_psbt(
+        self, psbt: bdk.PartiallySignedTransaction
+    ) -> bdk.PartiallySignedTransaction:
         "Returns a signed psbt. However it still needs to be finalized by  a bdk wallet"
         hwi_psbt = PSBT()
         hwi_psbt.deserialize(psbt.serialize())
-        
+
         signed_hwi_psbt = self.client.sign_tx(hwi_psbt)
-                
+
         return bdk.PartiallySignedTransaction(signed_hwi_psbt.serialize())
-    
-        
-    
-    def sign_message(self, message:str, bip32_path:str)->str:
+
+    def sign_message(self, message: str, bip32_path: str) -> str:
         return self.client.sign_message(message, bip32_path)
-        
 
-    def display_address(self,  descriptor_str:str, keychain:bdk.KeychainKind,  address_index:int,  network:bdk.Network)->str:
+    def display_address(
+        self,
+        descriptor_str: str,
+        keychain: bdk.KeychainKind,
+        address_index: int,
+        network: bdk.Network,
+    ) -> str:
         desc_infos = public_descriptor_info(descriptor_str)
-        
-
 
         if desc_infos.address_type.is_multisig:
-            pubkey_providers = [signer_info.to_hwi_pubkey_provider() for signer_info in desc_infos.signer_infos]
-            return self.client.display_multisig_address( get_hwi_address_type(desc_infos.address_type), HWIMultisigDescriptor(pubkey_providers, )
-                                                        
-                                                        )
+            pubkey_providers = [
+                signer_info.to_hwi_pubkey_provider()
+                for signer_info in desc_infos.signer_infos
+            ]
+            return self.client.display_multisig_address(
+                get_hwi_address_type(desc_infos.address_type),
+                HWIMultisigDescriptor(
+                    pubkey_providers, thresh=desc_infos.threshold, is_sorted=True
+                ),
+            )
         else:
-            return self.client.display_singlesig_address(desc_infos.address_type.get_bip32_path(network,keychain, address_index), get_hwi_address_type(desc_infos.address_type))
-        
+            return self.client.display_singlesig_address(
+                desc_infos.address_type.get_bip32_path(
+                    network, keychain, address_index
+                ),
+                get_hwi_address_type(desc_infos.address_type),
+            )
