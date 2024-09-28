d9305bdd1463fce761505973b73b0cc10235c574
andreasgriffin <andreasgriffin@proton.me>
2024-09-28 13:28:53 +0200

    simplified udev installation.  Now also works when called from an appimage

--- bitcoin_usb/udevwrapper.py
+++ bitcoin_usb/udevwrapper.py
@@ -4,15 +4,15 @@ import subprocess
 import sys
 import tempfile
 from pathlib import Path
-from typing import Optional
+from typing import List
 
-from hwilib.udevinstaller import UDevInstaller, _resource_path
+from hwilib.udevinstaller import _resource_path
 
 from bitcoin_usb.i18n import translate
 
 
 class UDevWrapper:
-    def find_filename_in_dir(self, filename: str, dir: Path) -> Optional[Path]:
+    def list_rule_files(self) -> List[Path]:
         """
         Searches for a file in the specified directory that matches the filename exactly.
         Returns the Path object for the file if found, otherwise None.
@@ -24,25 +24,16 @@ class UDevWrapper:
         Returns:
         Optional[Path]: The path to the file that matches the filename exactly, or None if no match is found.
         """
-        if not dir.is_dir():
-            return None
-
+        files: list[Path] = []
         # Iterate over all files in the directory
-        for file in dir.iterdir():
-            if file.is_file() and file.name == filename:
-                return file
+        for file in self.get_udev_source(absolute=True).iterdir():
+            if file.is_file() and file.name.endswith(".rules"):
+                files.append(file)
+        return files
 
-        # Return None if no file was found
-        return None
-
-    def get_udev_destination(
-        self,
-    ) -> Path:
-        return Path("/etc/udev/rules.d/")
-
-    def get_udev_source(self, absolute: bool) -> str:
-        source_dir = "udev"
-        return _resource_path(source_dir) if absolute else source_dir
+    def get_udev_source(self, absolute: bool) -> Path:
+        source_dir = Path("udev")
+        return Path(_resource_path(str(source_dir))) if absolute else source_dir
 
     def linux_execute_sudo_script(self, script_content: str):
         terminals = ["konsole", "gnome-terminal", "xterm", "lxterminal", "xfce4-terminal"]
@@ -84,80 +75,35 @@ class UDevWrapper:
 
         return True
 
-    def _linux_cmd_install_udev(
-        self,
-    ) -> bool:
-        "Needs root permission.  So actually execute it with sudo_linux_cmd_install_udev"
-        source_dir = self.get_udev_source(absolute=False)
-        destination = self.get_udev_destination()
-        installer = UDevInstaller()
-        print(f"Copies the rules from {self.get_udev_source(absolute=True)} to {destination}")
-        return installer.install(source=str(source_dir), location=str(destination))
+    @staticmethod
+    def copy_files(full_filenames: List[Path], target_dir: Path) -> None:
+        """
+        Copy a list of files to a target directory.
 
-    def linux_cmd_install_udev_as_sudo(self, sleep=3):
-        "Calls python as sudo and runs _linux_cmd_install_udev"
-        python_executable_path = sys.executable
+        Args:
+        full_filenames (List[Path]): A list of paths to files that need to be copied.
+        target_dir (Path): The directory to copy the files into.
+        """
+        for file_path in full_filenames:
+            if file_path.is_file():
+                shutil.copy(file_path, target_dir)
+                print(f"Copied {file_path} to {target_dir}")
+
+    def install_udev(self, sleep=3):
+        temp_dir = Path(tempfile.mkdtemp())
+
+        filename_install_script = "install_udev.sh"
+        files = self.list_rule_files() + [Path(__file__).absolute().parent / filename_install_script]
+        self.copy_files(files, temp_dir)
+
         script_content = f"""
                         #!/bin/bash
-                        sudo {python_executable_path} {Path(__file__).absolute()} --install_linux_udev
+                        sudo sh {(temp_dir/filename_install_script).absolute()} 
                         sleep {sleep}
                         """
         self.linux_execute_sudo_script(script_content)
 
-    def _all_files_present(self, source: Path, destination: Path, file_extension: str) -> bool:
-        """
-        Checks if all files with a specific file extension in the 'source' directory are present in the 'destination' directory.
-
-        Args:
-        source (Path): Path to the source directory.
-        destination (Path): Path to the destination directory.
-        file_ending (str): The file extension to filter by, e.g., '.rules'.
-
-        Returns:
-        bool: True if all files with the specified file extension from the source are present in the destination, False otherwise.
-        """
-        if not source.is_dir() or not destination.is_dir():
-            return False  # Return False if either path is not a directory
-
-        # Gather filenames with the specified file extension from source directory
-        source_files = {
-            file.name for file in source.iterdir() if file.is_file() and file.suffix == file_extension
-        }
-
-        # Gather filenames with the specified file extension from destination directory
-        destination_files = {
-            file.name for file in destination.iterdir() if file.is_file() and file.suffix == file_extension
-        }
-
-        # Check if all filtered files from the source are in the destination
-        return source_files <= destination_files  # Subset check
-
-    def all_udev_files_installed(self) -> bool:
-        source = Path(self.get_udev_source(absolute=True))
-        destination = self.get_udev_destination()
-
-        return self._all_files_present(source, destination, file_extension="rule")
-
 
 if __name__ == "__main__":
-    # since
-    import argparse
-
-    parser = argparse.ArgumentParser(description="Can install the udev rules")
-    parser.add_argument("--install_linux_udev", action="store_true")
-    parser.add_argument("--test", action="store_true")
-
-    args = parser.parse_args()
-
     udev_wrapper = UDevWrapper()
-    if args.install_linux_udev:
-        if udev_wrapper._linux_cmd_install_udev():
-            print(translate("bitcoin_usb", "Successfully installed the udev rules"))
-        else:
-            print(translate("bitcoin_usb", "Could not install udev rules"))
-
-        if udev_wrapper.all_udev_files_installed():
-            print(translate("bitcoin_usb", "All udev files were successfully copied"))
-
-    else:
-        udev_wrapper.linux_cmd_install_udev_as_sudo()
+    udev_wrapper.install_udev()
