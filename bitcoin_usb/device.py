53f30621677499f8c4391749f72b347b7dcf72e7
andreasgriffin <andreasgriffin@proton.me>
2025-01-31 16:01:44 +0100

    better buttons in question dialog

--- bitcoin_usb/device.py
+++ bitcoin_usb/device.py
@@ -322,7 +322,10 @@ class USBDevice(BaseDevice, QObject):
                 logger.error(f"{filepath} returned {error=}")
 
             if not self.client.client.features.initialized:
-                if question_dialog(text=self.tr("Do you want to restore an existing seed onto the device?")):
+                if question_dialog(
+                    text=self.tr("Do you want to restore an existing seed onto the device?"),
+                    buttons=QMessageBox.StandardButton.No | QMessageBox.StandardButton.Yes,
+                ):
                     self.client.restore_device(label=self.initalization_label)
                 else:
                     self.client.setup_device(label=self.initalization_label)
@@ -335,7 +338,10 @@ class USBDevice(BaseDevice, QObject):
             self.noise_config = DialogNoiseConfig()
             self.client.noise_config = self.noise_config
             if not self.is_bitbox02_initialized(self.client):
-                if question_dialog(text=self.tr("Do you want to restore an existing seed onto the device?")):
+                if question_dialog(
+                    text=self.tr("Do you want to restore an existing seed onto the device?"),
+                    buttons=QMessageBox.StandardButton.No | QMessageBox.StandardButton.Yes,
+                ):
                     self.client.restore_device(label=self.initalization_label)
                 else:
                     self.client.setup_device(label=self.initalization_label)
